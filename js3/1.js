cb368aaa8a0893519fbde491c166df9e60bac4a6
Charles <samannaphala@gmail.com>
2020-07-01 18:17:12 +0300

    solves 3-1

--- js3/1.js
+++ js3/1.js
@@ -4,10 +4,12 @@
  * @returns {array} arr
  */
 
-const solution = (arr, obj) => {
-  return []
+const solution = (arr, obj, result=[]) => {
+  arr.forEach(e => {
+    if (obj[e]) result.push(obj[e])
+  })
+  return result
 }
-
 module.exports = {
   solution
 }
