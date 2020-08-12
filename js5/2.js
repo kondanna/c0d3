b4d0930fd0a9718374edc552f722ac5bdd9b9318
charles ko <samannaphala@gmail.com>
2020-08-12 09:20:44 +0300

    solves 5-2

--- js5/2.js
+++ js5/2.js
@@ -1,67 +1,51 @@
 const express = require('express')
 const { exec } = require('child_process')
-const cors = require ('cors')
-
 const app = express()
 app.use(express.json())
-app.use(cors())
-
-app.options('/api/*', (req, res) => {
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Origin', req.headers.origin)
-    res.header('Allow-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Credentials' 
-    )
-    res.send('ok')
-})
 
 app.post('/api/commands', ({body: {command}}, res) => {
-    console.log(command)
-    exec(command, (err, stdout, stderr) => {
-        console.log(command)
+    const forbiddenCommands = ['sudo', 'rm', 'mkdir', 'touch', 'nano']
+    let forbiddenInput = false
+    forbiddenCommands.some(cmd => { 
+        if (command.includes(cmd)) {
+            forbiddenInput = true
+            return true
+        } 
+    }) // Array.some breaks from loop anytime it returns true
+    if (forbiddenInput) return res.json('You entered an invalid command.')
+    exec(command, (err, stdout, _stderr) => {
         if (err) console.log(err)
-        console.log(stdout, stderr)
-        res.json(stdout)
+        return res.json(stdout)
     })
 })
 
 app.get('/commands', (req, res) => {
     res.send(`
-        <style>
-
-        </style>
-        <body>
+        <div>
             <h1>Commands</h1>
             <h3>Available commands: ls, cat, pwd</h3>
             <input type="text" class="commandInput">
             <hr>
             <div class="display"></div>
-        </body>
+        </div>
         <script>
             const $commandInput = document.querySelector('.commandInput')
             const $display = document.querySelector('.display')
             $commandInput.onkeyup = (e) => {
                 if (e.key === 'Enter') {
-                    const command = $commandInput.value
-                    const firstKeyword = command.split(' ')[0] 
-                    if (firstKeyword === 'cat' || firstKeyword === 'ls' || firstKeyword === 'pwd') {
-                        fetch('http://localhost:3000/api/commands', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({ command })
-                        }).then(r => r.json()).then(data => {
-                            $display.innerText = data
-                            $commandInput.value = ''
-                        })
-                    }
+                    fetch('http://localhost:3000/api/commands', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ command: $commandInput.value })
+                    }).then(r => r.json()).then(data => {
+                        $display.innerText = data
+                        $commandInput.value = ''
+                    })
                 }
             }
-        </script>
-    `)
+        </script>`)
 })
 
 app.listen(3000)
