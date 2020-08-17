5b5fdb2afab4a42b813c9ed7f4ab638152488e57
charles ko <samannaphala@gmail.com>
2020-08-17 18:21:52 +0300

    progress

--- js5/c4/4.js
+++ js5/c4/4.js
@@ -1,26 +1,34 @@
 const express = require('express')
 const fs = require('fs')
+const { uuid } = require('uuidv4')
 
 app = express()
 app.use(express.static('public'))
+app.use(express.json())
 
-let filesData = {} // {'1597495994768': {name: 'test.js', createdAt: 1597495994768}, ...}
+const cache = {}
+let filesData = {} // {'some uuid': {name: 'test.js', lastSeen: 1597495994768}, ...}
 fs.readFile('./files.json', (_err, data) => {
     filesData = data 
 })
 
 app.post('/api/files', (req, res) => {
-    const {name, content} = req.body.data
-    const createdAt = Date.now() // also the file ID
-    filesData[createdAt] = {name, createdAt}
-    fs.writeFile(`./files/${name}`, content, () => { console.log(`New file created at ${createdAt}`)})
+    const {name, content} = req.body
+    const id = uuid()
+    const lastSeen = Date.now() 
+    filesData[id] = {name, lastSeen}
+    fs.writeFile(`./public/files/${name}`, content, () => { console.log('new file added')})
     fs.writeFile('./files.json', JSON.stringify(filesData), () => {})
 })
 
-app.get('/api/files/:filename', (req, res) => {
+app.get('/api/files/:filename', (req, res) => { 
     const fileName = req.params.filename
-    fs.readdir('./files', (_err, filesArr) => { // returns array of all filenames
-        
+    filesData[fileName]['lastSeen'] = Date.now()
+})
+
+app.get('/api/files', (req, res) => {// returns sorted array of all filenames
+    fs.readdir('./public/files', (_err, filesArr) => { 
+        res.json(JSON.stringify(filesArr.sort()))
     })
 })
 
