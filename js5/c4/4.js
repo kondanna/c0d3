5ea801112d8528a25680bb321942d9e99e046f8e
charles ko <samannaphala@gmail.com>
2020-08-18 09:28:02 +0300

    first draft server

--- js5/c4/4.js
+++ js5/c4/4.js
@@ -5,27 +5,37 @@ app = express()
 app.use(express.static('public'))
 app.use(express.json())
 
-const cache = {} // {<fileName>: <fileContents>, ...}
+// const cache = {} // {<fileName>: <fileContents>, ...}
 let filesData = {} // {<fileName>: {name: <fileName>, lastSeen: Date.now()}, ...}
 fs.readFile('./files.json', (_err, data) => {
     filesData = data 
 })
 
+const cleanFiles = () => {
+    const now = Date.now()
+    Object.keys(filesData).forEach(fileName => {
+        if (now - filesData[fileName]['lastSeen'] > 300000) {
+            fs.unlink(`./public/${fileName}`, (err) => {
+                return console.log(err)
+            })
+        } 
+    })
+    setTimeout(cleanFiles, 60000)
+}
+
 app.post('/api/files', (req, res) => {
     const {name, content} = req.body
     filesData[name] = {name, lastSeen: Date.now()}
-    cache[name] = content
     fs.writeFile(`./public/files/${name}`, content, () => { console.log('new file added')})
     fs.writeFile('./files.json', JSON.stringify(filesData), () => {})
+    res.json({name: fileName})
 })
 
 app.get('/api/files/:filename', (req, res) => { 
     const fileName = req.params.filename
-    const now = Date.now()
-    if (now - cache[fileName]['lastSeen'] < 300000) return res.json(cache[fileName]) //
     filesData[fileName]['lastSeen'] = Date.now()
     fs.readFile(`/public/files/${fileName}`, (_err, data) => {
-        res.json(data) //
+        res.json({name: fileName, content: data})
     })
 })
 
@@ -35,4 +45,4 @@ app.get('/api/files', (req, res) => {// returns sorted array of all filenames
     })
 })
 
-app.listen(3000)
+app.listen(3000, () => { cleanFiles() })
