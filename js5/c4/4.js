ff017d01b501ad7f6daf9ccafa4da4c3eb579c72
charles ko <samannaphala@gmail.com>
2020-08-19 12:52:23 +0300

    solves 5-4

--- js5/c4/4.js
+++ js5/c4/4.js
@@ -5,16 +5,14 @@ app = express()
 app.use(express.static('public'))
 app.use(express.json())
 
-// const cache = {} // {<fileName>: <fileContents>, ...}
 let filesData = {} // {<fileName>: {name: <fileName>, lastSeen: Date.now()}, ...}
-fs.readFile('./files.json', (_err, data) => {
-    filesData = data 
-})
 
 const cleanFiles = () => {
+    const now = Date.now()
     Object.keys(filesData).forEach(fileName => {
-        if (Date.now() - filesData[fileName]['lastSeen'] > 300000) {
-            fs.unlink(`./public/${fileName}`, () => {})
+        if (now - filesData[fileName]['lastSeen'] > 300000) {
+            console.log('Removing file: ', fileName)
+            fs.unlink(`./public/files/${fileName}`, () => {})
             delete filesData[fileName]
         } 
     })
@@ -27,14 +25,14 @@ app.post('/api/files', (req, res) => {
     filesData[name] = {name, lastSeen: Date.now()}
     fs.writeFile(`./public/files/${name}`, content, () => { console.log('new file added')})
     fs.writeFile('./files.json', JSON.stringify(filesData), () => {})
-    res.json({name: fileName})
+    res.json({name})
 })
 
 app.get('/api/files/:filename', (req, res) => { 
     const fileName = req.params.filename
     filesData[fileName]['lastSeen'] = Date.now()
-    fs.readFile(`/public/files/${fileName}`, (_err, data) => {
-        res.json({name: fileName, content: data})
+    fs.readFile(`./public/files/${fileName}`, (_err, data) => {
+        res.json({name: fileName, content: data.toString()})
     })
 })
 
@@ -44,4 +42,9 @@ app.get('/api/files', (req, res) => {
     })
 })
 
-app.listen(3000, cleanFiles)
+app.listen(3000, () => {
+    fs.readFile('./files.json', (_err, data) => {
+        filesData = JSON.parse(data)
+    })
+    cleanFiles()
+})
