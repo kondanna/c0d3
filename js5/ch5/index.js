03ce4f615399d17cff12eace92c309c97a181f93
charles ko <samannaphala@gmail.com>
2020-08-24 10:05:44 +0300

    changes caching layer 5-5

--- js5/ch5/index.js
+++ js5/ch5/index.js
@@ -5,37 +5,29 @@ const fetch = require('node-fetch')
 const app = express()
 app.use(express.static('public'))
 app.use(express.json())
+
+let messages = {} // {<roomName>: [{<user>: <message>}, ...], ...}
 app.use('/api', (req, res, next) => {
     fetch('https://js5.c0d3.com/auth/api/session', {
         headers: {
             Authorization: req.get('Authorization')
         }
     }).then(r => r.json()).then(data => {
-        if (!data || !data.username) {
-            return res.status(403).json({
-                message: 'User not authorized'
-            })
-        }
+        if (!data || !data.username) return res.status(403).json('Not Authorized')
         req.userInfo = data
         next()
     }) 
 })
 
-let messages = {} // {<roomName>: [{<user>, <message>}, ...], ...}
-
 app.get('/api/:room/messages', (req, res) => {
-    fs.readFile('./messages.json', (err, data) => {
-        if (err) return console.log(err)
-        messages = JSON.parse(data)
-    })
-    res.json(messages[req.params.room])
+    const roomName = req.params.room
+    messages[roomName] = messages[roomName] || []
+    res.json(messages[roomName])
 })
 
 app.post('/api/:room/messages', (req, res) => {
     const roomName = req.params.room
-    messages[roomName] = messages[roomName] || []
-    messages[roomName].push({user: req.userInfo.username, message: req.body.message})
-    console.log(messages)
+    messages[roomName] = (messages[roomName] || []).push({user: req.userInfo.username, message: req.body.message})
     fs.writeFile(`./messages.json`, JSON.stringify(messages), () => console.log('added message'))
     res.json(messages[roomName])
 })
