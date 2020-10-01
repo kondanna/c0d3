e8e9d75872437d22a0e3a07f5c6798e3a636fabd
Charles Ko <samannaphala@gmail.com>
2020-10-01 19:03:55 +0300

    kanban

--- js6/ch3/index.js
+++ js6/ch3/index.js
@@ -1,12 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/stars', (req, res) => {
-    res.sendFile(`${__dirname}/stars.html`)
-})
+app.use(express.static("public"))
 
-app.get('/kanban', (req, res) => {
-    res.sendFile(`${__dirname}/kanban.html`)
-})
-
-app.listen(3000, () => console.log('listening on 3000'))
+app.listen(3000)
