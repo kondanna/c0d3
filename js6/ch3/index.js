940d1871a550b563df18f8ee7ce6d6dbd2049e01
Charles Ko <samannaphala@gmail.com>
2020-10-02 21:28:51 +0300

    changed localstorage getter

--- js6/ch3/index.js
+++ js6/ch3/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.use(express.static("public"))
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: __dirname })
+})
 
 app.listen(3000)
