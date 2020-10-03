cba2e9989169779c3eea322311a3744cb4b5060e
Charles Ko <samannaphala@gmail.com>
2020-10-03 22:41:07 +0300

    deleted node modules before committing

--- js6/ch3/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: __dirname })
-})
-
-app.listen(3000)
