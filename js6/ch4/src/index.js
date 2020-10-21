d2d3e3b5611f43029f7d95c320229299f76a2a5c
Charles Ko <samannaphala@gmail.com>
2020-10-21 20:03:56 +0300

    solves 6-4 webpack

--- js6/ch4/src/index.js
+++ js6/ch4/src/index.js
@@ -23,57 +23,60 @@ const sendQuery = query => {
     }).then(r => r.json()).then(r => r.data)
 }
 
-const Profile = props => {
-    const [enrolled, setEnrolled] = useState([])
-    const [notEnrolled, setNotEnrolled] = useState([])
+const Profile = ({ user }) => {
+    const [enrollment, setEnrollment] = useState({})
 
     useEffect(() => {
-        sendQuery(`{user {name, image, lessons {title}}, lessons {title}}`).then(({ user, lessons }) => {
+        sendQuery(`{lessons {title}}`).then(({ lessons }) => {
+            const lessonMap = lessons.reduce((acc, lesson) => {
+                acc[lesson.title] = { enrolled: false }
+                return acc
+            }, {})
             const userLessons = user.lessons || []
-            const enrolledLessons = userLessons.map(lesson => lesson.title) // array of lesson objects => array of titles
-            const notEnrolledLessons = lessons.filter(e => !enrolledLessons.includes(e))
-            setEnrolled(enrolledLessons)
-            setNotEnrolled(notEnrolledLessons)
+            userLessons.forEach(lesson => {
+                lessonMap[lesson.title].enrolled = true
+            })
+            setEnrollment(lessonMap)
         })
     }, [])
 
     const handleEnroll = title => {
         sendQuery(`mutation {enroll(title: "${title}") {title}}`).then(_ => {
-            setEnrolled([...enrolled, title])
+            setEnrollment({ ...enrollment, [title]: { enrolled: true } })
         })
     }
 
     const handleUnenroll = title => {
         sendQuery(`mutation {unenroll(title: "${title}") {title}}`).then(_ => {
-            setNotEnrolled(enrolled.filter(lesson => lesson !== title))
+            setEnrollment({ ...enrollment, [title]: { enrolled: false } })
         })
     }
 
     const handleLogout = () => {
         sendQuery(`{login (str:"") {name}}`).then(_ => {
-            return <Search />
+            window.location.reload()
         })
     }
 
     return (
         <div>
-            <h1>{props.name}</h1>
-            <img src={props.image}/>
-            <button onClick={() => handleLogout()}></button>
+            <h1>{user.name}</h1>
+            <img src={user.image} />
+            <button onClick={() => handleLogout()}>Logout</button>
 
-            <hr/>
+            <hr />
             <div>
                 <h2>Enrolled</h2>
                 <p>Click to unenroll</p>
-                {enrolled.map(lesson => 
-                <h4 id={lesson.title} onClick={e => handleUnenroll(e.target.id)}>{lesson.title}</h4>)}
+                {Object.keys(enrollment).filter(title => enrollment[title].enrolled).map((title, i) =>
+                    <h4 key={i} id={title} onClick={() => handleUnenroll(title)}>{title}</h4>)}
             </div>
-            <hr/>
+            <hr />
             <div>
                 <h2>Not Enrolled</h2>
                 <p>Click to enroll</p>
-                {notEnrolled.map(lesson => 
-                <h4 id={lesson.title} onClick={e => handleEnroll(e.target.id)}>{lesson.title}</h4>)}
+                {Object.keys(enrollment).filter(title => !enrollment[title].enrolled).map((title, i) =>
+                    <h4 key={i} id={title} onClick={() => handleEnroll(title)}>{title}</h4>)}
             </div>
         </div>
     )
@@ -87,62 +90,64 @@ const Search = () => {
     const handleKeyUp = e => {
         if (e.key === 'Enter') return loadSelection(searchText)
 
-        console.log('keyup')
         debounce(() => {
+            console.log('keyup')
             sendQuery(`{search(str:"${searchText}") {name}}`).then(data => {
                 const results = data.search || []
                 setSuggestions(results)
             })
-        }, 300)
+        }, 300)()
     }
-    
+
     const loadSelection = name => {
         setSearchText('')
         sendQuery(`{getPokemon(str:"${name}"){name, image}}`).then(result => {
-            console.log(result)
-            setSelectedPokemon(result)
+            console.log(result.getPokemon)
+            setSelectedPokemon(result.getPokemon)
+            setSuggestions([])
         })
     }
 
     const handleLogin = name => {
-        sendQuery(`{login (str:"${name}") {name}}`).then(result => {
-            return <Profile user={result.name}/>
+        sendQuery(`{login (str:"${name}") {name}}`).then(_ => {
+            window.location.reload()
         })
     }
 
     return (
         <div>
             <h1>Pokemon Search</h1>
-            <input type="text" value={searchText} onkeyup={e => { handleKeyUp(e)}} style="width: 100%;"/>
-            <hr/>
-            { selectedPokemon ? 
-            <div>{suggestions.map(pokemon => <h3 onclick={() => {loadSelection(pokemon)}}>{pokemon.name}</h3>)}</div> :
-            <div>
-                <h1>${selectedPokemon.name}</h1>
-                <img src={selectedPokemon.image} />
-                <button onclick={() => handleLogin(selectedPokemon.name)}>Login</button>
-            </div> }
+            <input type="text" value={searchText} onChange={e => setSearchText(e.target.value)} onKeyUp={e => handleKeyUp(e)} style={{ width: "100%" }} />
+            <hr />
+            <div>{suggestions.map((pokemon, i) => <h3 key={i} style={{ cursor: 'pointer' }} onClick={() => loadSelection(pokemon.name)}>{pokemon.name}</h3>)}</div>
+            { selectedPokemon.name
+                ? <div>
+                    <h1>{selectedPokemon.name}</h1>
+                    <img src={selectedPokemon.image} />
+                    <button onClick={() => handleLogin(selectedPokemon.name)}>Login</button>
+                </div>
+                : null}
         </div>
     )
 }
 
 const App = () => {
-    [currentUser, setCurrentUser] = useEffect('')
+    const [currentUser, setCurrentUser] = useState({})
 
     useEffect(() => {
-        sendQuery(`{user {name, image, lessons {title}}, lessons {title}}`).then(({ user, lessons }) => {
-            console.log(user)
-            if (!user) return 
-            setCurrentUser(user) // user = array of enrolled lessons, lessons = array of all lessons
+        sendQuery(`{user {name, image, lessons {title}}}`).then(({ user }) => {
+            if (!user) return
+            setCurrentUser(user)
         })
     }, [])
 
     return (
         <div>
-            { currentUser ? <Profile user={currentUser}/> : <Search /> }
+            { Object.keys(currentUser).length === 0
+                ? <Search />
+                : <Profile user={currentUser} />}
         </div>
     )
-    
 }
 
 ReactDOM.render(<App />, $root)
