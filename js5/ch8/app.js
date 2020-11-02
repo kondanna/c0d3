97ae557df6861bd9e96c08cdf2e4981de25695ab
Charles Ko <samannaphala@gmail.com>
2020-11-02 19:44:57 +0200

    starting 6-5

--- js5/ch8/app.js
+++ js5/ch8/app.js
@@ -11,10 +11,12 @@ const images = {} // {<uuid>: {<base64Data>, <url>}, ...}
 app.get('/api/images', (_req, res) => {
     fs.readdir('./public/images', (_err, data) => {
         res.status(200).json(data)
-    })
+    }) 
 })
 
 app.post('/api/images', (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
     const imgID = uuidv4()
     images[imgID] = {img: req.img, url: `/public/images/${imgID}.png`}
     fs.writeFile(`./public/images/${imgID}.png`, req.img, 'base64', () => { 
