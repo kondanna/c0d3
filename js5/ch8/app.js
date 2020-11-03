8b629169c210e2076f9faeeba0b12641fe7948b5
Charles Ko <samannaphala@gmail.com>
2020-11-03 19:45:49 +0200

    solves 6-5

--- js5/ch8/app.js
+++ js5/ch8/app.js
@@ -3,25 +3,24 @@ const { v4: uuidv4 } = require('uuid')
 const fs = require('fs')
 
 const app = express()
-app.use(express.json({limit: '10mb'}))
+app.use(express.json({ limit: '10mb' }))
 app.use(express.static('public'))
 
 const images = {} // {<uuid>: {<base64Data>, <url>}, ...}
 
-app.get('/api/images', (_req, res) => {
+app.get('/api/images', (req, res) => {
     fs.readdir('./public/images', (_err, data) => {
         res.status(200).json(data)
-    }) 
+    })
 })
 
 app.post('/api/images', (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
     const imgID = uuidv4()
-    images[imgID] = {img: req.img, url: `/public/images/${imgID}.png`}
-    fs.writeFile(`./public/images/${imgID}.png`, req.img, 'base64', () => { 
-        console.log('image saved') 
-        res.status(201).json({id: imgID})
+    const img = req.body.img
+    const url = `/public/images/${imgID}.png`
+    images[imgID] = { img, url }
+    fs.writeFile(`.${url}`, img, 'base64', () => {
+        res.status(201).json({ img, url })
     })
 })
 
