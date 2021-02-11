a6490c478dedd2bd01800d677a63342352799625
Charles <samannaphala@gmail.com>
2021-02-11 16:40:51 +0200

    submitted js6/ch7

--- js6/c7/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-import App from './App'
-
-const client = new ApolloClient({ uri: '/graphql', cache: new InMemoryCache() })
-
-ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, $root)
